55f4df9c04ba83366f7272a0290565955a79b36f
Erlend Fonnes <erlend.fonnes@gmail.com>
2021-05-09 14:40:26 +0200

    Improved parallelism in pgrid

--- consts.h
+++ consts.h
@@ -1,12 +1,12 @@
 #ifndef CONSTS_H
 #define CONSTS_H
 
-#define num_points 5000
+#define num_points 200000
 #define num_clusters 30
 #define dims 3
 #define max_num 25000  // The maximum size of the points (with clustered points, the max will be 1.01 times max_num)
 #define decimal 100    // Makes points with decimals equal to the number of zeros in decimal
-#define num_cells 10   // Number of cells in the grid (in each dimension)
+#define num_cells 20   // Number of cells in the grid (in each dimension)
 
 typedef struct Point {
   long double coords[dims];
--- pgrid.h
+++ pgrid.h
@@ -9,7 +9,6 @@
 #include "consts.h"
 #include "utils.h"
 
+void pgrid_calc(Point *points, Point grid[], Point *clusters, int *belongs_to, int num_cell_corners, int num_grid_cells, int num_corners, int *grid_points_closest, int grid_corners[][(int)(pow(2, dims) + 0.5)], int *cell_closest_cluster, int belongs_to_cell[]);
 void pinit_grid(int num_grid_cells, int num_cell_corners, int grid_corners[][(int)(pow(2, dims) + 0.5)], Point grid[], Point points[], int belongs_to_cell[]);
-void pgrid_closest_cluster(Point grid[], Point *clusters, int num_cell_corners, int num_grid_cells, int num_corners, int *grid_points_closest, int grid_corners[][(int)(pow(2, dims) + 0.5)], int *cell_closest_cluster);
-void pgrid_calc_belongs_to(Point *points, Point *clusters, int *belongs_to, int *cell_closest_cluster, int belongs_to_cell[]);
 #endif
